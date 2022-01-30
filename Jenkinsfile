4833aaa6b3c37c75e3377325a8e2e7621480e30c
cjuan-code <cjuaniestacio@gmail.com>
2022-01-30 19:24:02 +0100

    update update_readme app and test stage fix

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,10 +29,16 @@ pipeline {
         stage('Test') {
             steps {
 
+                // script {
+                //     env.cypress_test_result = sh(script: "npm run cypress_run_test", returnStdout: true).trim()
+                // }
                 script {
-                    env.cypress_test_result = sh(script: "npm run cypress_run_test", returnStdout: true).trim()
+                    sh ' npm run cypress_run_test > test_result'
+
+                    result = readFile('test_result').trim()
+
+                    env.cypress_test_result = result 
                 }
-                // sh ' npm run cypress_run_test'
             }
         }
 
--- jenkinsScripts/update_readme/index.js
+++ jenkinsScripts/update_readme/index.js
@@ -7,7 +7,7 @@ console.log('result', result)
 var regex = new RegExp(/<!---Start place for the badge -->\n(.*)\n<!---End place for the badge -->/g);
     
 if (result == "success") {
-    var file = fs.readFileSync("../../README.md", "utf-8");
+    var file = fs.readFileSync("./README.md", "utf-8");
 
     // var newstr = file.replace(regex, "<!---Start place for the badge -->\n" + "RESULTADO DE LOS ÚLTIMOS TESTS " + "[![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)\n" + "<!---End place for the badge -->")
 
@@ -16,7 +16,7 @@ if (result == "success") {
     console.log('success', file)
 
 } else if (result == "failure") {
-    var file = fs.readFileSync("../../README.md", "utf-8");
+    var file = fs.readFileSync("./README.md", "utf-8");
 
     // var newstr = file.replace(regex, "<!---Start place for the badge -->\n" + "RESULTADO DE LOS ÚLTIMOS TESTS " + "[![Cypress.io](https://img.shields.io/badge/test-failure-red)](https://www.cypress.io/)\n" + "<!---End place for the badge -->")
 
