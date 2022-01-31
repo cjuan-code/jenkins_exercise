65729b790e04d5243c89b19c3b68c5cb87945eb9
cjuan-code <cjuaniestacio@gmail.com>
2022-02-01 00:43:28 +0100

    Push_changes script update

--- jenkinsScripts/push_changes/push_changes.sh
+++ jenkinsScripts/push_changes/push_changes.sh
@@ -1,7 +1,7 @@
 git config --global user.name "cjuan-code"
 git config --global user.email "cjuaniestacio@gmail.com"
-git remote add origin $3
+git remote set-url origin $3
 
 git add README.md
 git commit -m "Pipeline ejecutada por $1. Motivo: $2"
-git push origin master
+git push origin HEAD:master
