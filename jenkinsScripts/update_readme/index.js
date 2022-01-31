e95115f9b6fa18eb9aec48717d22bda65c6cd4db
cjuan-code <cjuaniestacio@gmail.com>
2022-01-31 19:33:34 +0100

    update jenkinsfile and update_Readme app

--- jenkinsScripts/update_readme/index.js
+++ jenkinsScripts/update_readme/index.js
@@ -2,11 +2,11 @@ var fs = require('fs');
 
 var result = process.argv[2];
 
-console.log('result', result)
+// console.log('result', result)
 
 var regex = new RegExp(/<!---Start place for the badge -->\n(.*)\n<!---End place for the badge -->/g);
     
-if (result == "success") {
+if (result == 0) {
     var file = fs.readFileSync("./README.md", "utf-8");
 
     // var newstr = file.replace(regex, "<!---Start place for the badge -->\n" + "RESULTADO DE LOS ÚLTIMOS TESTS " + "[![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)\n" + "<!---End place for the badge -->")
@@ -15,7 +15,7 @@ if (result == "success") {
 
     console.log('success', file)
 
-} else if (result == "failure") {
+} else if (result == 1) {
     var file = fs.readFileSync("./README.md", "utf-8");
 
     // var newstr = file.replace(regex, "<!---Start place for the badge -->\n" + "RESULTADO DE LOS ÚLTIMOS TESTS " + "[![Cypress.io](https://img.shields.io/badge/test-failure-red)](https://www.cypress.io/)\n" + "<!---End place for the badge -->")
